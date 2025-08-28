a4e82e4b5195472661836e938f77bb2dbc4c9feb
Kezia Claudia Rohani Simanjuntak <keziasimanjuntak01@gmail.com>
2025-08-28 10:09:42 +0700

    add method put

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
+    // CREATE (POST /users)
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -26,6 +27,7 @@ class UserController extends Controller
         return response()->json($user, 201);
     }
 
+    // READ by ID (GET /users/{id})
     public function show($id)
     {
         $user = User::findOrFail($id);
@@ -33,10 +35,45 @@ class UserController extends Controller
         return response()->json($user);
     }
 
+    // READ all (GET /users)
     public function index()
     {
         $users = User::all();
         Log::info("GET /users", $users->toArray());
         return response()->json($users);
+    }
+
+    // UPDATE (PUT /users/{id})
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string',
+            'email' => 'sometimes|email|unique:users,email,' . $id,
+            'gender' => 'sometimes|in:male,female',
+            'password' => 'nullable|min:6',
+        ]);
+
+        if (isset($validated['password'])) {
+            $validated['password'] = Hash::make($validated['password']);
+        }
+
+        $user->update($validated);
+
+        Log::info("PUT /users/{$id}", $user->toArray());
+
+        return response()->json($user);
+    }
+
+    // DELETE (DELETE /users/{id})
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        Log::info("DELETE /users/{$id}");
+
+        return response()->json(['message' => 'User deleted successfully']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
 Route::get('/users', [UserController::class, 'index']);
